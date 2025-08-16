a36cf2141a184551aa3fbfb2a55c98b7d894ed1a
Gautam Arora <gtmdhoni@bbdu.ac.in>
2025-08-16 21:34:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsoption ={
-    origin:'http://localhost:5173',
+    origin:'https://hs-frontend.onrender.com',
     credentials: true
 }
 app.use(cors(corsoption));
